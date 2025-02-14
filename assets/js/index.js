c3ffcf3a37eac529e2af2417285ba1fde4a31ce7
wdaz <haqverdi.r@gmail.com>
2025-02-14 23:32:24 +0400

    Enhance navigation menu interaction by adding scrollIntoView on link click and defer script loading for improved performance

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ const navMenuLink = document.querySelectorAll('.nav-menu__link');
 navMenuLink.forEach((link, index) => {
     link.addEventListener('click', () => {
         navMenuLink.forEach((l, i) => i !== index ? l.classList.remove('active') : null);
-        link.classList.add('active')
+        link.classList.add('active');
+        link.scrollIntoView();
 
     })
 })
