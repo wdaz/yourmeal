b404bbcf0a5bd1749148b68376afb1f554319ee8
wdaz <haqverdi.r@gmail.com>
2025-02-14 22:42:42 +0400

    Refactor navigation menu structure and styles for improved clarity and functionality

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const navMenuLink = document.querySelectorAll('.nav-menu__link');
+navMenuLink.forEach((link, index) => {
+    link.addEventListener('click', () => {
+        navMenuLink.forEach((l, i) => i !== index ? l.classList.remove('active') : null);
+        link.classList.add('active')
+
+    })
+})
