21dea1dd4a15acc0031b42bd13e842d94882699e
wdaz <haqverdi.r@gmail.com>
2025-02-16 01:53:35 +0400

    Refactor burger section to dynamically display items and update price for improved accuracy

--- assets/js/index.js
+++ assets/js/index.js
@@ -57,7 +57,7 @@ const items = {
         {
             imgSrc: 'assets/images/food/burgers/burger.png',
             name: 'Мясная бомба',
-            price: 689,
+            price: 700,
             currency: '₽',
             weight: 520,
             uom: 'г',
@@ -84,14 +84,18 @@ const items = {
     ]
 }
 const navMenu = document.querySelector('.nav-menu');
+const section = document.querySelector('section');
 init();
 
 function init() {
     let menuItem = '';
     menus.forEach((menu, index) => {
+        if (index === 0) {
+            addItem(menu.id, menu.name);
+        }
         menuItem +=
             `<li class="nav-menu__item">
-                <a onclick="addEventToLink(${index})" 
+                <a onclick="addEventToLink('${menu.id}','${menu.name}',${index})" 
                 class="${index === 0 ? 'nav-menu__link active' : 'nav-menu__link'}">
                     <img src="${menu.imgSrc}" alt="${menu.name}">
                     <span>${menu.name}</span>
@@ -102,10 +106,32 @@ function init() {
     navMenu.innerHTML = menuItem;
 }
 
-function addEventToLink(index) {
+function addEventToLink(menuId, name, index) {
     const navMenuLink = document.querySelectorAll('.nav-menu__link');
     navMenuLink.forEach((l, i) => {
-        i !== index ? l.classList.remove('active') : (l.classList.add('active'), l.scrollIntoView());
+        i !== index ? l.classList.remove('active')
+            : (l.classList.add('active'), l.scrollIntoView(), addItem(menuId, name));
     });
 
+}
+
+function addItem(itemId, name) {
+    const selectedItems = items[itemId];
+
+    section.innerHTML = `
+          <h2>${name}</h2>
+            <div class="box">
+            ${selectedItems?selectedItems.reduce((updated, latest) =>
+        updated.concat(`
+                    <article class="item">
+                        <img src="${latest.imgSrc}" alt="${latest.name}" class="item__img">
+                        <p class="item__price">${latest.price}${latest.currency}</p>
+                        <p class="item__name">${latest.name}</p>
+                        <p class="item__weight">${latest.weight}${latest.uom}</p>
+                        <button type="button" class="item__add">Добавить</button>
+                    </article>
+                `
+        ), ''): 'Ничего не найдено'}
+            </div>
+            `
 }
